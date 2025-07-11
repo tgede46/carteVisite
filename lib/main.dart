55f3a139a272299b6c5f667d07093712f7e99310
tgede46 <gedeonkpara@gmail.com>
2025-07-11 11:34:09 +0000

    "field: navigationbart"

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,54 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex=0;
+
+  setCurrentIndex(int index){
+    setState(() {
+      _currentIndex=index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('carte de visite'),
+        ),
+        body: [
+          Home(),
+          Card(),
+        ][_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index)=> setCurrentIndex(index),
+          selectedItemColor: Colors.blue,
+          unselectedItemColor:  Colors.grey,
+          elevation: 10,
+          iconSize: 32,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.credit_card),
+                label: 'Card'
+            ),
+          ],
+        ),
       ),
-      home: const Home(),
     );
   }
 }
+
 
