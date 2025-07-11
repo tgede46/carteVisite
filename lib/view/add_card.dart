e093a63c92bb0adbf4cbabb161b4302b25caedc1
tgede46 <gedeonkpara@gmail.com>
2025-07-11 14:56:28 +0000

    "field: formulaire"

--- lib/view/add_card.dart
+++ lib/view/add_card.dart
@@ -44,7 +44,9 @@ class _AddCardState extends State<AddCard> {
                 decoration: InputDecoration(
                   labelText: "Name",
                   hintText: "Entrez votre Nom",
-                  border: OutlineInputBorder()
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
                 ),
                 validator: (value){
                   if(value==null||value.isEmpty){
@@ -62,7 +64,9 @@ class _AddCardState extends State<AddCard> {
                 decoration: InputDecoration(
                     labelText: "Title",
                     hintText: "Entrez votre metier",
-                    border: OutlineInputBorder()
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
                 ),
                 validator: (value){
                   if(value==null||value.isEmpty){
@@ -79,7 +83,9 @@ class _AddCardState extends State<AddCard> {
                 decoration: InputDecoration(
                     labelText: "Company",
                     hintText: "Acme corp",
-                    border: OutlineInputBorder()
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
                 ),
                 validator: (value){
                   if(value==null||value.isEmpty){
@@ -96,7 +102,9 @@ class _AddCardState extends State<AddCard> {
                 decoration: InputDecoration(
                     labelText: "Phone ",
                     hintText: "+1 (555)123-4567",
-                    border: OutlineInputBorder()
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
                 ),
                 validator: (value){
                   if(value==null||value.isEmpty){
@@ -107,12 +115,53 @@ class _AddCardState extends State<AddCard> {
                 controller: phoneCardController,
               ),
             ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "john.doe@xexample.com",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+                validator: (value){
+                  if(value==null||value.isEmpty){
+                    return "vous devez remplir ce champ";
+                  }
+                  return null;
+                },
+                controller: mailcardController,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Website",
+                  hintText: "WWW.example.com",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+                validator: (value){
+                  if(value==null||value.isEmpty){
+                    return "vous devez remplir ce champ";
+                  }
+                  return null;
+                },
+                controller: websiteCardController,
+              ),
+            ),
             SizedBox(
               child: ElevatedButton(
                   onPressed: (){
                     if(_formKey.currentState!.validate()){
                       final nameCard=nameCardController.text;
                       final titleCard=titleCardController.text;
+                      final companyCard=companyCardController.text;
+                      final phoneCard=phoneCardController.text;
+                      final mailCard=mailcardController.text;
 
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(content: Text("Envoi en cours... "))
